c0699cc52f05ca8087c09872ce547ba238bf0ecd
wanlf <wanlf@wanlfdeMacBook-Air.local>
2026-04-20 22:42:50 +0800

    feat: add download directory settings

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,7 +1,13 @@
 #include "mainwindow.h"
 #include <QVBoxLayout>
+#include <QHBoxLayout>
 #include <QLabel>
 #include <QWidget>
+#include <QPushButton>
+#include <QSettings>
+#include <QFileDialog>
+#include <QDir>
+#include "network/transfer.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -19,6 +25,22 @@ MainWindow::MainWindow(QWidget *parent)
     // Receiver tab
     QWidget *receiverWidget = new QWidget;
     QVBoxLayout *receiverLayout = new QVBoxLayout(receiverWidget);
+
+    // Download directory display and change button
+    QSettings settings;
+    QString downloadDir = settings.value("downloadDir", QDir::home().filePath("Downloads/FileSwitch")).toString();
+
+    m_transfer = new FileTransfer(this);
+    m_transfer->setDownloadDirectory(downloadDir);
+
+    QLabel *dirLabel = new QLabel(QString("下载目录: %1").arg(downloadDir));
+    QPushButton *changeBtn = new QPushButton("更改");
+
+    QHBoxLayout *dirLayout = new QHBoxLayout;
+    dirLayout->addWidget(dirLabel);
+    dirLayout->addWidget(changeBtn);
+
+    receiverLayout->addLayout(dirLayout);
     receiverLayout->addWidget(new QLabel("Receiver Mode - placeholder"));
     receiverWidget->setLayout(receiverLayout);
 
@@ -27,4 +49,29 @@ MainWindow::MainWindow(QWidget *parent)
 
     setCentralWidget(m_tabWidget);
     resize(600, 500);
+
+    connect(changeBtn, &QPushButton::clicked, this, &MainWindow::onChangeDownloadDir);
+}
+
+void MainWindow::onChangeDownloadDir()
+{
+    QSettings settings;
+    QString currentDir = m_transfer->downloadDirectory();
+    QString newDir = QFileDialog::getExistingDirectory(this, "选择下载目录", currentDir);
+
+    if (!newDir.isEmpty()) {
+        m_transfer->setDownloadDirectory(newDir);
+        settings.setValue("downloadDir", newDir);
+
+        // Update label
+        QWidget *receiverWidget = m_tabWidget->widget(1);
+        QLayout *layout = receiverWidget->layout();
+        if (QLayoutItem *item = layout->itemAt(0)) {
+            if (QHBoxLayout *hLayout = item->layout()) {
+                if (QLabel *label = qobject_cast<QLabel *>(hLayout->itemAt(0)->widget())) {
+                    label->setText(QString("下载目录: %1").arg(newDir));
+                }
+            }
+        }
+    }
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,11 +2,17 @@
 #include <QMainWindow>
 #include <QTabWidget>
 
+class FileTransfer;
+
 class MainWindow : public QMainWindow {
     Q_OBJECT
 public:
     explicit MainWindow(QWidget *parent = nullptr);
 
+private slots:
+    void onChangeDownloadDir();
+
 private:
     QTabWidget *m_tabWidget;
+    FileTransfer *m_transfer;
 };
